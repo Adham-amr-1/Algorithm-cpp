72db186458dc7edd946bfa10778678e63ac57b72
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-04-10 21:52:39 +0200

    Create Simple Array Sum.cpp

---
+++ Simple Array Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, i, sum = 0;
+    cin >> n;
+    int* arr = new int[n];
+    for (i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    cout << sum;
+
+    return 0;
+}
